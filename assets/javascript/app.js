5719f562685c134a068e0051d663c4960d6e986b
BreBusby <brebusby13@gmail.com>
2018-03-19 03:41:56 -0500

    lightbulb update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,51 @@
 $( document ).ready(function() {
     // An array of actions, new actions will be pushed into this array;
-    var topics = ["Skittles", "Gummy Bears", "Smarties", "Sour Patch Kids", "Starbust", "Air Heads", "Mike and Ikes", "Lemonheads", "Twizzlers", "Gummy Worms","Jolly Ranchers", "Snickers", "Dots"];
-    // Creating Functions & Methods
-    // Function that displays gif buttons
-    function displayGifButtons(){
-        $("#gifButtonsView").empty();
-        for (var i = 0; i < topics.length; i++){
-            var gifButton = $("<button>");
-            gifButton.addClass("candy");
-            gifButton.addClass("btn btn-primary")
-            gifButton.attr("data-name", topics [i]);
-            gifButton.text(topics [i]);
-            $("#gifButtonsView").append(gifButton);
-        }
-    }
-       // Function to add a new candy button
+    var topics = ["Skittles", "Gummy Bears", "Smarties", "Sour Patch Kids", "Starbust", "Air Heads", "Mike and Ikes", "Lemonheads", "Twizzlers", "Gummy Worms"];
+
+  // Function to display gifs and ratings
+    $("button").on("click", function() {
+
+        var topics = $(this).attr("data-name");
+  
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        topics + "&api_key=V00lp6evlnUIr8X0GVKqXTKTyvlGjQc3&limit=10"
+
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+
+          .then(function(response) {
+
+            var results = response.data;
+  
+            for (var i = 0; i < results.length; i++) {
+  
+              if (results[i].rating !== "r" && results[i].rating !== "g") {
+
+                var gifDiv = $("<div class='item'>");
+
+                var rating = results[i].rating;
+  
+                var c = $("<p>").text("Rating: " + rating);
+  
+                var candyImage = $("<img>");
+  
+                candyImage.attr("src", results[i].images.fixed_height.url);
+  
+                gifDiv.append(c);
+                gifDiv.append(candyImage);
+  
+                $("#gifsView").prepend(gifDiv);
+              }
+            }
+          });
+     
+          // Function to add a new candy button
        function addNewButton(){
         $("#addGif").on("click", function(){
         var candy = $("#candy-input").val().trim();
-        if (candy == ""){
+        if (candy === ""){
           return false;
         }
         topics.push(candy);
@@ -28,57 +55,7 @@ $( document ).ready(function() {
         });
     }
 
-    // Function that displays all of the gifs
-    function displayGifs(){
-        var candy = $(this).attr("data-name");
-        var queryURL = "http://" + candy + "V00lp6evlnUIr8X0GVKqXTKTyvlGjQc3";
-        console.log(queryURL); 
-        $.ajax({
-            url: queryURL,
-            method: 'GET'
-        })
 
-        .done(function(response) {
-            console.log(response); 
-            $("#gifsView").empty(); 
-            var results = response.data;
-            if (results == ""){
-              alert("There isn't a gif for this selected button");
-            }
-            for (var i=0; i<results.length; i++){
-    
-                var gifDiv = $("<div>");
-                gifDiv.addClass("gifDiv");
-
-                var gifRating = $("<p>").text("Rating: " + results[i].rating);
-                gifDiv.append(gifRating);
-
-                var gifImage = $("<img>");
-                gifImage.attr("src", results[i].images.fixed_height_small_still.url); 
-                gifImage.attr("data-still",results[i].images.fixed_height_small_still.url);
-                gifImage.attr("data-animate",results[i].images.fixed_height_small.url);
-                gifImage.attr("data-state", "still");
-                gifImage.addClass("image");
-                gifDiv.append(gifImage);
-                $("#gifsView").prepend(gifDiv);
-            }
-        });
-    }
-   
-    displayGifButtons(); 
-    addNewButton();
-    removeLastButton();
-   
-    $(document).on("click", ".action", displayGifs);
-    $(document).on("click", ".image", function(){
-        var state = $(this).attr('data-state');
-        if ( state == 'still'){
-            $(this).attr('src', $(this).data('animate'));
-            $(this).attr('data-state', 'animate');
-        }else{
-            $(this).attr('src', $(this).data('still'));
-            $(this).attr('data-state', 'still');
-        }
     });
-    });
+});
     
